9d0b597c55e7cab28686d5ee06655ea64552cedb
juanes2374 <jueposadama@unal.edu.co>
2025-05-24 15:54:35 -0500

    Final 2

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -1,22 +1,22 @@
 import os
 import subprocess
+import sys
 import warnings
 
 warnings.filterwarnings("ignore")
 
 def test_01():
     try:
-        subprocess.run(
-            [
-                "cmd", "/c", "run.bat"
-            ],
-            check=True,
-        )
+        if sys.platform == "win32":
+            # Windows: ejecuta run.bat
+            subprocess.run(["cmd", "/c", "run.bat"], check=True)
+        else:
+            # Linux/Mac: ejecuta run.sh
+            subprocess.run(["bash", "run.sh"], check=True)
     except subprocess.CalledProcessError as e:
         raise Exception(f"Error running the homework script: {e}")
 
     assert os.path.exists("mlruns"), "mlruns directory does not exist."
-
     experiments = [
         d for d in os.listdir("mlruns") if os.path.isdir(os.path.join("mlruns", d))
     ]
